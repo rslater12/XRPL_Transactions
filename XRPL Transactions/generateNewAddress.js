09604b50175a4c59f516a96d71f7a0db185e9d6b
Richard Slater <slater.richard@hotmail.co.uk>
2021-08-01 21:17:55 +0100

    Update generateNewAddress.js

--- XRPL Transactions/generateNewAddress.js
+++ XRPL Transactions/generateNewAddress.js
@@ -1,5 +1,17 @@
 const RippleAPI = require('ripple-lib').RippleAPI;
-
 const api = new RippleAPI({server: 'wss://s.altnet.rippletest.net:51233'});
+var fs = require('fs')
 
-console.log(api.generateAddress())
+async function generateAddress(){
+var Address = await api.generateAddress()
+  
+fs.appendFile('List of Testnet Address.txt', Address.address+'\n', function (err) {
+    if (err) throw err;
+  }); 
+  fs.appendFile('List of Testnet Address.txt', Address.secret+'\n\n', function (err) {
+    if (err) throw err;
+    console.log(Address,' has been Saved!');
+  }); 
+}
+
+generateAddress()
